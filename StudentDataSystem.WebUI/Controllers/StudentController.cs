a61303cec96c226f1eaff2290e2d817b10324de6
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-12-04 13:51:10 +0300

    student added,listed and deleted completed

--- StudentDataSystem.WebUI/Controllers/StudentController.cs
+++ StudentDataSystem.WebUI/Controllers/StudentController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentDataSystem.WebUI.DataAccess;
+using StudentDataSystem.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,17 +9,64 @@ using System.Threading.Tasks;
 
 namespace StudentDataSystem.WebUI.Controllers
 {
-    public class StudentController:Controller
+    public class StudentController : Controller
     {
+
+
+        private readonly StudentDataSystemContext _context;
+        public StudentController(StudentDataSystemContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
 
-            return View();
+            List<Student> students = new List<Student>();
+            students = _context.Set<Student>().ToList();
+            if (students != null)
+            {
+                return View(students);
+            }
+            else
+            {
+                return View();
+            }
         }
         public IActionResult AddStudent()
         {
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student model)
+        {
+
+            if (model != null)
+            {
+
+                Student student = new Student();
+                student.Address = model.Address;
+                student.BirthDate = model.BirthDate;
+                student.BloodType = model.BloodType;
+                student.Department = model.Department;
+                student.Email = model.Email;
+                student.HesCode = model.HesCode;
+                student.IdentityNumber = model.IdentityNumber;
+                student.Name = model.Name;
+                student.PhoneNumber = model.PhoneNumber;
+                student.StudentNumber = model.StudentNumber;
+                student.Surname = model.Surname;
+                _context.Students.Add(student);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+        public IActionResult DeleteStudent(int id)
+        {
+            _context.Set<Student>().Remove(new Student { Id = id });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
